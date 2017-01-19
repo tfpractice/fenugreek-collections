ffdc1b0cebf16ae207536d015f1dec062c5afef9
theory friction practice <jackson.brebnor@gmail.com>
2017-01-19 14:19:14 -0500

    casting works default fix

--- __tests__/cast.test.js
+++ __tests__/cast.test.js
@@ -1,23 +1,23 @@
-// import 'jasmine-expect';
-//
-// import { asArray, asMap, asSet, } from 'src/cast';
-// const qSet = new Set([ 1, 2, 3, 4, ]);
-// const qMap = new Map().set(8, 1).set(7, 2).set(6, 3).set(5, 4);
-// const qArr = [ 0, 11, 22, 33, 44, ];
-//
-// describe('cast', () => {
-//   describe('asArray', () => {
-//     it('returns an array representation of a collection', () => {
-//       expect(asArray(qMap)).toBeArray();
-//     });
-//   });
-//   describe('asMap', () => {
-//     it('returns an array representation of a collection', () => {
-//       expect(asMap(qSet) instanceof Map).toBeTrue();
-//     });
-//   }); describe('asSet', () => {
-//     it('returns an array representation of a collection', () => {
-//       expect(asSet(qArr) instanceof Set).toBeTrue();
-//     });
-//   });
-// });
+import 'jasmine-expect';
+
+import { asArray, asMap, asSet, } from 'src/cast';
+const qSet = new Set([ 1, 2, 3, 4, ]);
+const qMap = new Map().set(8, 1).set(7, 2).set(6, 3).set(5, 4);
+const qArr = [ 0, 11, 22, 33, 44, ];
+
+describe('cast', () => {
+  describe('asArray', () => {
+    it('returns an array representation of a collection', () => {
+      expect(asArray(qMap)).toBeArray();
+    });
+  });
+  describe('asMap', () => {
+    it('returns an array representation of a collection', () => {
+      expect(asMap(qSet) instanceof Map).toBeTrue();
+    });
+  }); describe('asSet', () => {
+    it('returns an array representation of a collection', () => {
+      expect(asSet(qArr) instanceof Set).toBeTrue();
+    });
+  });
+});
--- src/cast.js
+++ src/cast.js
@@ -1,9 +1,5 @@
-import { spread, spreadKV, } from './spread';
+import spread, { spreadKV, } from './spread';
 
-export const asArray = coll => spread(coll);
-export const asSet = coll => new Set(spread(coll));
-export const asMap = coll => new Map(spreadKV(coll));
-
-console.log('asArray(10)', asArray(10));
-
- // export default
+export const asArray = spread;
+export const asSet = c => new Set(spread(c));
+export const asMap = c => new Map(spreadKV(c));
