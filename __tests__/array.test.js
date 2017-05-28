4f9348c40ee82a4ae1d19b2765cfab9259d95b88
theory friction practice <jackson.brebnor@gmail.com>
2017-05-28 18:56:23 -0400

    adds permuted array functiions

--- __tests__/array.test.js
+++ __tests__/array.test.js
@@ -1,12 +1,18 @@
 import 'jasmine-expect';
-import { every, filter, map, reduce, some, } from 'src/array';
+import { every, everyPass, filtBy, filter, map, mapTo, reduce, some, somePass, } from 'src/array';
 
-const nums = [ 1,2,3,4,5, ];
-const logAndSum = (p, n) => { console.log('p,v', p,n); return p + n; };
+const nums = [ 1, 2, 3, 4, 5, ];
+const logAndSum = (p, n) => { console.log('p,v', p, n); return p + n; };
 
 describe('every', () => {
   it('checks if every element returns true for the callback', () => {
     expect(every(nums)(Number.isInteger)).toBeTrue();
+  });
+});
+
+describe('everyPass', () => {
+  it('checks if every element returns true for the callback', () => {
+    expect(everyPass(Number.isInteger)(nums)).toBeTrue();
   });
 });
 
@@ -15,14 +21,25 @@ describe('filter', () => {
     expect(filter(nums)(n => n % 2 === 0)).toBeArray();
   });
 });
+
+describe('filtBy', () => {
+  it('returns the eleements of the colection which  return true for the callback ', () => {
+    expect(filtBy(n => n % 2 === 0)(nums)).toBeArray();
+  });
+});
 describe('map', () => {
   it('returns a new array of the elements mapped to a function', () => {
     expect(map(nums)(v => v * 2)).toContain(10);
   });
 });
+describe('mapTo', () => {
+  it('returns a new array of the elements mapped to a function', () => {
+    expect(mapTo(v => v * 2)(nums)).toContain(10);
+  });
+});
 
 describe('reduce', () => {
   it('reduces the elements in an array to a single value determined by the callback', () => {
-    expect(reduce(nums)((acc, v) => acc + v,0)).toEqual(15);
+    expect(reduce(nums)((acc, v) => acc + v, 0)).toEqual(15);
   });
 });
--- src/array.js
+++ src/array.js
@@ -6,6 +6,8 @@ import { spread, } from './spread';
 // function called on each element of an iterable 
 export const map = coll => fn => spread(coll).map(fn);
 
+export const mapTo = fn => coll => map(coll)(fn);
+
 // **reduce** `:: Iterable<a>  -> ((a->b), b) -> b`  
 // returns the accumulated value of a function
 // called on each element of an iterable 
@@ -15,10 +17,16 @@ export const reduce = coll => (fn, init) => spread(coll).reduce(fn, init);
 // returns the iterable's values which return true for a given function
 export const filter = coll => fn => spread(coll).filter(fn);
 
+export const filtBy = fn => coll => filter(coll)(fn);
+
 // **every** `:: Iterable<a>  -> (a->bool) -> bool`  
 // checks if every element of an iterable returns true for a given function
 export const every = coll => fn => spread(coll).every(fn);
 
+export const everyPass = fn => coll => every(coll)(fn);
+
 // **some** `:: Iterable<a>  -> (a->b) -> [b]`  
 // checks if any element of an iterable returns true for a given function
 export const some = coll => fn => spread(coll).some(fn);
+
+export const somePass = fn => coll => some(coll)(fn);
