534cc0001c9c48ccbb80233aa83fd58656abca2b
theory friction practice <jackson.brebnor@gmail.com>
2017-06-12 20:49:04 -0400

    length and match

--- src/spread.js
+++ src/spread.js
@@ -1,23 +1,23 @@
 // requires [iterify](iterable.html)
 import { iterify, } from './iterable';
 
-// **spread** `:: Iterable<a> -> Iterable<a>`  
+// **spread** `:: Iterable<a> -> Iterable<a>`
 // returns an Iterable<a> of the collections default iterator
 export const spread = (coll = []) => [ ...iterify(coll), ];
 
-// **spreadK** `:: Iterable<a> -> Iterable<a>`  
+// **spreadK** `:: Iterable<a> -> Iterable<a>`
 // returns an Iterable<a> of the collections keys
 export const spreadK = (coll = []) => spread(iterify(coll).keys());
 
-// **spreadV** `:: Iterable<a> -> Iterable<a>`  
+// **spreadV** `:: Iterable<a> -> Iterable<a>`
 // returns an Iterable<a> of the collections values
 export const spreadV = (coll = []) => spread(iterify(coll).values());
 
-// **spreadE** `:: Iterable<a> -> Iterable<a>`  
+// **spreadE** `:: Iterable<a> -> Iterable<a>`
 // returns an Iterable<a> of the collections entries
 export const spreadE = (coll = []) => spread(iterify(coll).entries());
 
-// **spreadKV** `:: Iterable<a> -> Iterable<a>`  
+// **spreadKV** `:: Iterable<a> -> Iterable<a>`
 // returns an Iterable<a> of the collections entries
 export const spreadKV = spreadE;
 
